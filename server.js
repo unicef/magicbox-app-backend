a7b0ad9216f8ecd7879743567f6755ca28e22e0c
Marcella <marcellamaki@gmail.com>
2019-04-15 16:59:37 -0400

    add routes folder and initial files

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
-var express    = require('express');
-var app        = express();
-var bodyParser = require('body-parser');
+const express    = require('express');
+const app        = express();
+const bodyParser = require('body-parser');
+
+import routes from './routes';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
-
-var router = express.Router();
+const port = process.env.PORT || 8080;
+const router = express.Router();
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
+// REGISTER ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
+app.use('/api', routes.index);
+app.use('/api/configs', routes.config);
+app.use('/api/population_points', routes.population_point);
+app.use('/api/schools', routes.school);
+app.use('/api/shapes', routes.shape);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
