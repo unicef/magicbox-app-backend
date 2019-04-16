ddcf650c2465e4ee8fc02916d3d6388e8679ea41
Marcella <marcellamaki@gmail.com>
2019-04-16 11:01:35 -0400

    remove unnecessary lines and comments

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const shapefiles = require('./routes/shapefiles');
 const schools = require('./routes/schools');
 const population_points = require('./routes/population_points');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.json({ limit: '250mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }))
 
@@ -18,7 +16,5 @@ app.use('/api/configs', configs);
 app.use('/api/schools', schools);
 app.use('/api/population_points', population_points);
 app.use('/api/shapefiles', shapefiles);
-
-// app.use('/api', router);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
