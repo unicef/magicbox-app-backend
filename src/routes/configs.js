cecf3c500b4b13fb87f6337085848bd93e8206b9
Marcella <marcellamaki@gmail.com>
2019-04-16 16:36:46 -0400

    add sample admin boundaries to public and update route to serve from public, fix typo with config

--- src/routes/configs.js
+++ src/routes/configs.js
@@ -1,17 +1,16 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const fs = require('fs')
 const jsonfile = require('jsonfile');
 
 router.get('/', (req, res) => {
     const path = './public/sample-data/sample_colombia_config.json';
     return new Promise(function(resolve) {
         jsonfile.readFile(path, (err, file) => {
-          resolve(file)
-        })
+            resolve(file);
+        });
     })
-    .then(result => res.send(result));
+        .then(result => res.send(result));
 });
 
 module.exports = router;
--- src/routes/shapefiles.js
+++ src/routes/shapefiles.js
@@ -1,8 +1,16 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const jsonfile = require('jsonfile');
 
-router.get('/', (req, res) => { return res.send('hello world from shapefiles global!'); });
-router.get('/:countryCode', (req, res) => { return res.send('hello world from shapefiles one country!'); });
+router.get('/', (req, res) => {
+    const path = './public/sample-data/sample_colombia_admin_0.json';
+    return new Promise(function(resolve) {
+        jsonfile.readFile(path, (err, file) => {
+            resolve(file);
+        });
+    })
+        .then(result => res.send(result));
+});
 
 module.exports = router;
