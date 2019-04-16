468de22cec2fdedc018bbcd3dff9d69d0d37ceb8
Marcella <marcellamaki@gmail.com>
2019-04-16 16:35:46 -0400

    add sample config to public and update route to serve from public

--- src/routes/configs.js
+++ src/routes/configs.js
@@ -1,8 +1,17 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const fs = require('fs')
+const jsonfile = require('jsonfile');
 
-router.get('/', (req, res) => { return res.send('hello world from configs global!'); });
-router.get('/:countryCode', (req, res) => { return res.send('hello world from configs one country!'); });
+router.get('/', (req, res) => {
+    const path = './public/sample-data/sample_colombia_config.json';
+    return new Promise(function(resolve) {
+        jsonfile.readFile(path, (err, file) => {
+          resolve(file)
+        })
+    })
+    .then(result => res.send(result));
+});
 
 module.exports = router;
