ae81aeb16e0181b9c04c9a1a3b66207aa8a94055
Marcella <marcellamaki@gmail.com>
2019-04-17 15:01:09 -0400

    (WIP) testing out functions to aggregate get data from multiple endpoints

---
+++ src/helpers/assemble-config.js
@@ -0,0 +1,36 @@
+const request = require('request');
+
+// promisify the request module
+function requestAsync(url) {
+    return new Promise(function(resolve, reject) {
+        request(url, function(err, response, body) {
+            if (err) {
+                reject(err);
+            } else if (response.statusCode !== 200) {
+                reject(new Error("response.statusCode = " + response.statusCode));
+            } else {
+                resolve(body);
+            }
+        });
+    });
+}
+// multiple endpoints to retrieve data from
+function getData(endpoints) {
+    return Promise.all(endpoints.map(function(url) {
+        return requestAsync(url);
+    }));
+}
+
+let endpoints = [
+  'http://localhost:5000/api/configs',
+  'http://localhost:5000/api/schools',
+  'http://localhost:5000/api/population-points',
+  'http://localhost:5000/api/admin_boundaries'
+]
+
+getData(endpoints).then(function(results) {
+    console.log(results)
+    // do some sort of results assembly into the config here
+}).catch(function(err) {
+    console.log(err)
+});
