06ed7886470752d829448306e119909f7b188f0c
Marcella <marcellamaki@gmail.com>
2019-05-01 13:50:12 -0700

    updated view endpoint to work with connected database

--- src/routes/views.js
+++ src/routes/views.js
@@ -1,23 +1,13 @@
 const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
-const jsonfile = require('jsonfile');
-const helper = require('../helpers/assemble-config');
-
-// the view endpoint should be what controls the view that is compiled.
-//  - First, it should return the view object containing id, domain/url, theme_id and data_id
-//  - then, it should control, either directly or with a helper, the fetching of two key datasets
-//      - the theme, an object that will contain the app/theme config
-//      - the data, which contains the data that is displayed (or pointers to the data that is displayed)
+const mongoUtil = require('../mongoUtil');
 
 router.get('/', (req, res) => { // eslint-disable-line
-    const path = '../public/sample-data/sample_colombia_view.json';
-    return new Promise(function(resolve) {
-        jsonfile.readFile(path, (err, file) => {
-            resolve(file);
-        });
-    })
-        .then(result => res.send(result));
+    const db = mongoUtil.getDb();
+    db.collection('view').find().toArray((err, items) => {
+        console.log(items)
+  })
 });
 
 module.exports = router;
