4a48781f9fec123a4b32d5bb8c6d9de62377b4a5
Marcella <marcellamaki@gmail.com>
2019-05-29 13:59:38 -0400

    add front end url env variable to server.js for CORS list

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv');
+const myEnv = dotenv.config();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const index = require('./routes/index');
 const views = require('./routes/views');
 const mongoUtil = require('./mongoUtil');
 
-
 mongoUtil.connectToServer( function( err, client ) {
   if (err) console.log(err);
   console.log('connected!');
 } );
-//
-// const clearlist = ['http://localhost:8080']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (clearlist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+
+const clearlist = [`${process.env.FRONTEND_URL}`]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (clearlist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 // Then pass them to cors:
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '250mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }));
 
