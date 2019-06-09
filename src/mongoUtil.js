b8c19b3cd7463316c5b2d5a9e3d5eeac6c48ad64
mikefab <mikefabrikant@gmail.com>
2019-06-09 15:36:21 -0400

    Protect mongo. This is okay, better if user/pass not in connection URL. TODO.

--- src/mongoUtil.js
+++ src/mongoUtil.js
@@ -1,19 +1,17 @@
 const MongoClient = require( 'mongodb' ).MongoClient;
-const url = `mongodb://${process.env.MONGO_IP}:27017`;
-
+const url = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGO_IP}:27017`;
 const dbName = 'myproject';
-let _db
-console.log(url)
+let _db;
 module.exports = {
 
-  connectToServer: function( callback ) {
-    MongoClient.connect( url,  { useNewUrlParser: true }, function( err, client ) {
-      _db  = client.db(dbName);
-      return callback( err );
-    } );
-  },
+    connectToServer: function( callback ) {
+        MongoClient.connect( url,  { useNewUrlParser: true }, function( err, client ) {
+            _db  = client.db(dbName);
+            return callback( err );
+        } );
+    },
 
-  getDb: function() {
-    return _db;
-  }
+    getDb: function() {
+        return _db;
+    }
 };
