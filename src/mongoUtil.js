470e5b68ada26b77004eaf9caabea683d4f3688d
mikefab <mikefabrikant@gmail.com>
2019-07-11 13:03:27 +0000

    Note about mongo security

--- src/mongoUtil.js
+++ src/mongoUtil.js
@@ -1,4 +1,7 @@
 const MongoClient = require( 'mongodb' ).MongoClient;
+
+// Need to replace this method of mongo login
+// to avoid credentials printing out in server logs
 const url = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGO_IP}:27017`;
 const dbName = 'myproject';
 let _db;
